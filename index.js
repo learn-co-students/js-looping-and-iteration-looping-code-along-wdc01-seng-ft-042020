9e0fdc25425da4d7687e0a7b6e339000f07cda85
jlangley3 <jlangleyiii@aol.com>
2020-05-30 16:01:10 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+// const array = ["Ada", "Brendan", "Ali"]
+// const event = "birthday"
+
+function writeCards(array, event) {
+    let new_array = [];
+    for (let i = 0; i < array.length; i++) {
+        new_array.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`)
+    }
+    return new_array
+}
+
+function countDown(num) {
+    let i = 0;
+    while (i <= num) {
+        console.log(i++)
+    };
+}
