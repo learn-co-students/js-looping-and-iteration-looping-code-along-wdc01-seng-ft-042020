b24f000e6a9d0b2b542bef00c445a6a72bd6d1c2
epittab <ep7m@me.com>
2020-05-31 13:31:34 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function writeCards(arrOfNames, eventName) {
+    const messagesArray = [];
+    for (let i = 0; i < arrOfNames.length; i++ ) {
+        messagesArray.push(`Thank you, ${arrOfNames[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return messagesArray;
+}
+
+function countDown(positiveInt) {
+    while (positiveInt >= 0) {
+        console.log(positiveInt)
+        positiveInt--;;
+    }
+}
