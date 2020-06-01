a9acc2f0e1eba9a152b37f862e28c4995222f886
agdsarge <agdsargeant@gmail.com>
2020-06-01 04:55:16 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+// writeCards(["Ada", "Brendan", "Ali"], "birthday");
+
+let ret = []
+function writeCards(ary, str) {
+    for (let ind = 0; ind < ary.length; ind++) {
+        let sent = `Thank you, ${ary[ind]}, for the wonderful ${str} gift!`;
+        ret.push(sent);
+    }
+
+    return ret;
+}
+// console.log(writeCards(["Ada", "Brendan", "Ali"], "birthday"));
+
+
+function countDown(nat) {
+    let count = nat
+    while (count >= 0) {
+        console.log(count)
+        count--
+    }
+}
