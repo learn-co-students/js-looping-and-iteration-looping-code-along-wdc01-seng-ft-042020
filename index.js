3859666461602dad2ee7e7210abb1d60067755f6
asharma414 <abhi.sharm.414@gmail.com>
2020-05-31 20:46:52 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    let arr = []
+    for (let i = 0; i < names.length; i++) {
+        arr.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return arr
+}
+
+function countDown(num) {
+    for (let i = num; i >= 0; i--) {
+        console.log(i)
+    }
+}
